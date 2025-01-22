0c5cbbbb65583a1e7533ef4f2a23954f1782130b
URIAN VIERA <urian1213viera@gmail.com>
2025-01-22 17:28:30 -0500

    iniciando git

---
+++ README.md
@@ -0,0 +1,28 @@
+# Eliminar Registro de Tabla con Svelte
+
+Este proyecto es un ejemplo simple de cómo eliminar registros de una tabla utilizando el framework Svelte. La tabla muestra una lista de personas y permite eliminar un registro al hacer clic en el ícono de la papelera.
+
+#### Resultado final
+![](https://raw.githubusercontent.com/urian121/imagenes-proyectos-github/refs/heads/master/consumir-data-desde-archivo-JSON-con-Svelte.png)
+
+
+## Tecnologías utilizadas
+
+- **Svelte**: Framework de JavaScript para crear aplicaciones interactivas.
+- **Bootstrap**: Framework CSS para diseño responsivo y componentes estilizados.
+- **Bootstrap Icons**: Librería de íconos basada en Bootstrap.
+
+
+# Apóyanos 🙌
+
+✨ **Comparte este proyecto** con otros desarrolladores para que puedan beneficiarse 📢.
+
+☕ **Invítame un café o una cerveza 🍺**:
+   - [Paypal](https://www.paypal.me/iamdeveloper86) (`iamdeveloper86@gmail.com`).
+
+👍 **Suscríbete a mi canal de [YouTube](https://www.youtube.com/WebDeveloperUrianViera?sub_confirmation=1)** para más contenido increíble y tutoriales.
+
+⭐ **Déjanos una estrella en GitHub**:
+   - Dicen que trae buena suerte 🍀.
+
+Gracias por tu apoyo 🤓.
---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+  import "bootstrap/dist/css/bootstrap.min.css";
+  import "bootstrap-icons/font/bootstrap-icons.css";
+
+  import { onMount } from "svelte";
+  let personas = [];
+
+  // Cargar los datos al montar el componente
+  onMount(async () => {
+    const response = await fetch("/data.json"); // Asumiendo que el archivo .json está en la raíz pública
+    personas = await response.json();
+  });
+
+  function eliminarRegistro(idPersona) {
+    console.log("Eliminar registro", idPersona);
+    personas = personas.filter((persona) => persona.id !== idPersona);
+  }
+</script>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1 class="text-center fw-bold mb-5">
+        Eliminar registro de una tabla con svelte <hr />
+      </h1>
+
+      <div class="table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Teléfono</th>
+              <th scope="col">Sexo</th>
+              <th scope="col">Profesión</th>
+              <th scope="col">Departamento</th>
+              <th scope="col">Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {#each personas as persona}
+              <tr id={persona.id}>
+                <th>{persona.id}</th>
+                <td>{persona.nombre}</td>
+                <td>{persona.telefono}</td>
+                <td>{persona.Sexo}</td>
+                <td>{persona.profesion}</td>
+                <td>{persona.departamento}</td>
+                <td>
+                  <button
+                    class="btn btn-warning"
+                    type="button"
+                    on:click={() => eliminarRegistro(persona.id)}
+                    aria-label="Eliminar registro"
+                  >
+                    <i class="bi bi-trash"></i>
+                  </button>
+                </td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
